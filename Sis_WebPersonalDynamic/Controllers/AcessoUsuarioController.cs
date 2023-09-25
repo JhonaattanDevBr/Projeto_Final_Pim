893ce23e7b098560578c5edc835546abf3648846
Iara Mota <iaramota243@gmail.com>
2023-09-25 12:34:18 -0300

    Alteração e finalização da tela inicial e adição de itens no menu sendo esses faltas e envio de atestado

--- Sis_WebPersonalDynamic/Controllers/AcessoUsuarioController.cs
+++ Sis_WebPersonalDynamic/Controllers/AcessoUsuarioController.cs
@@ -23,10 +23,6 @@ namespace Sis_WebPersonalDynamic.Controllers
             return View();
         }
          
-        public IActionResult EnviarAtestado()
-        {
-            return View();
-        }   
 
         public IActionResult SolicitaFerias()
         {
