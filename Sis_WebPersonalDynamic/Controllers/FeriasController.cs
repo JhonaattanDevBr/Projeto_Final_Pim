3bbcf5eb51ca938d82f30d13e5aebbd1148dbb89
Iara Mota <iaramota243@gmail.com>
2023-09-26 13:31:14 -0300

    subindo alterações nas views e controllers

---
+++ Sis_WebPersonalDynamic/Controllers/FeriasController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Sis_WebPersonalDynamic.Controllers
+{
+    public class FeriasController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
