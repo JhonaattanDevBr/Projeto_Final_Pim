3b36968a1553803b3356b35fd50dba3eeb6fa5f6
Jhonatan <jhon.devbr@gmail.com>
2023-10-13 01:22:57 -0300

    Enviando atualizações

---
+++ ConexaoBaseDados/crud_Funcionarios.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConexaoBaseDados
+{
+    public class crud_Funcionarios
+    {
+
+        public Dictionary<int, string> PopularCaixaConvenioMedico()
+        {
+            string caminho = @"Data Source=DESKTOP-AF6EDUF\SQLEXPRESSS;Initial Catalog=Base_Dados_Personal_Teste;Integrated Security=True";
+            SqlConnection conexaoDb = new SqlConnection(caminho);
+
+            try
+            {
+                conexaoDb.Open();
+
+                string query = "SELECT Id_saude, Nome FROM Planos_saude ORDER BY Id_saude";
+                SqlCommand cmd = new SqlCommand(query, conexaoDb);
+
+                SqlDataReader leitura = cmd.ExecuteReader();
+
+                // Criando um Dictionary para armazenar os dados do banco de dados
+                Dictionary<int, string> dados = new Dictionary<int, string>();
+
+                while (leitura.Read())
+                {
+                    int idSaude = leitura.GetInt32(0);  // Lendo o Id_saude (assume-se que é int)
+                    string nome = leitura.GetString(1); // Lendo o Nome
+
+                    // Adicionando os dados ao Dictionary
+                    dados[idSaude] = nome;
+                }
+
+                // Retornando o Dictionary com os dados do banco de dados
+                conexaoDb.Close();
+                return dados;
+
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+    }
+}
--- InterfacesDoSistemaDesktop/Program.cs
+++ InterfacesDoSistemaDesktop/Program.cs
@@ -19,7 +19,7 @@ namespace InterfacesDoSistemaDesktop
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form_Acesso());
+            Application.Run(new Form_CadFuncionarios());
         }
     }
 }
