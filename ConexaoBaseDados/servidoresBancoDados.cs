ffefce9e17dc46b19ff7e1d92b460c6f79134ce9
Jhonatan <jhon.devbr@gmail.com>
2023-10-20 21:22:33 -0300

    Recebendo alterações

--- ConexaoBaseDados/servidoresBancoDados.cs
+++ ConexaoBaseDados/servidoresBancoDados.cs
@@ -10,8 +10,7 @@ namespace ConexaoBaseDados
 {
     public class servidoresBancoDados
     {
-        public string servidorNotebook = @"Data Source=JHONATAN\SQLEXPRESS;Initial Catalog=Base_Dados_Personal_Teste;Integrated Security=True";
-        public string servidorCasa = @"";
-        // Ola
+        //public string servidorNotebook = @"Data Source=JHONATAN\SQLEXPRESS;Initial Catalog=Base_Dados_Personal_Teste;Integrated Security=True";
+        public string servidorNotebook = @"Data Source=DESKTOP-AF6EDUF\SQLEXPRESSS;Initial Catalog=Base_Dados_Personal_Teste;Integrated Security=True"; //servidor do meu desktop.
     }
 }
