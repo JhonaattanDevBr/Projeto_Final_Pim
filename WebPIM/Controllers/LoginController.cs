2ee4d7995c817c6070954b2cfe3afaf8ac4b1572
Jhonatan <jhon.devbr@gmail.com>
2023-10-30 21:57:02 -0300

    Subindo projeto atualizado + WEB

---
+++ WebPIM/Controllers/LoginController.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Data.SqlClient;
+using WebPIM.Models;
+
+namespace WebPIM.Controllers
+{
+    public class LoginController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+            public ActionResult Login(LoginModel login)
+            {
+                // Verifique se o email e a senha correspondem a um registro válido no banco de dados
+                string query = "SELECT COUNT(*) FROM Cadastro WHERE Email = @email AND Senha = @senha";
+                int count = 0;
+
+                using (SqlConnection connection = new SqlConnection(@"Data Source=LAPTOP-TJ6127TR;Initial Catalog=Base_Dados_Personal_Dynamic;Integrated Security=True"))
+                {
+                    connection.Open();
+
+                    using (SqlCommand command = new SqlCommand(query, connection))
+                    {
+                        command.Parameters.AddWithValue("@email", login.Email);
+                        command.Parameters.AddWithValue("@senha", login.Senha);
+                        count = (int)command.ExecuteScalar();
+                    }
+                }
+                if (count == 1)
+                {
+                    // Credenciais válidas, redirecione para a página Index
+                    return RedirectToAction("Index", "Home");
+                }
+                else
+                {
+                    // Credenciais inválidas, exiba uma mensagem de erro
+                    TempData["MensagemErro"] = "Email ou senha inválidos";
+                    //ModelState.AddModelError("", "Email ou senha inválidos.");
+                    return View(login);
+                }
+            }
+    }
+}
---
+++ WebPIM/Models/AgendamentoFeriasModel.cs
@@ -0,0 +1,17 @@
+﻿namespace WebPIM.Models
+{
+    public class AgendamentoFeriasModel
+    {
+        public int Id_agendamento { get; set; }
+        public int PrimeiroPeriodo { get; set; }
+        public int PrimeiroMes { get; set; }
+        public int SegundoPeriodo { get; set; }
+        public int SegundoMes { get; set; }
+        public int TerceiroPeriodo { get; set; }
+        public int TerceiroMes { get; set; }
+        public int Dias_restantes { get; set; }
+        public int Dias_vendidos { get; set; }
+        public int Primeira_parcela_decimo { get; set; }
+
+    }
+}
